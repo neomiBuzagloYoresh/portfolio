81ad6011bce734cf58a9553ee800dc65ba598304
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-02-07 23:12:38 +0200

    final fix

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -43,7 +43,7 @@ function renderModal() {
 
     var strHTMLs = gProjs.map((project,indx) => {
         return `
-        <div class="continer">
+        <div class="container">
         <div
           class="portfolio-modal modal fade"
           id="portfolioModal${indx+1}"
@@ -80,15 +80,14 @@ function renderModal() {
                       <ul class="list-inline">
                         <li>Date: January 2017</li>
                         <li>Date: January 2017</li>
+                        </ul>
+      
+       
+                        <a href="${project.url}" target="_blank">
+                         <button type="button" class="btn btn-success" >Check it Out</button>
+                         </a>
+                     
                       
-                      </ul>
-                     <button
-                      class="btn btn-primary "
-                      type="button"
-                    >
-                      <a href="${project.url}" target="_blank">
-                     Proj
-                    </button>
                       <button
                         class="btn btn-primary"
                         data-dismiss="modal"
@@ -107,10 +106,63 @@ function renderModal() {
         </div>
       </div>`});
 
-    document.querySelector('.modal-continer').innerHTML = strHTMLs.join('');
+    document.querySelector('.modal-container').innerHTML = strHTMLs.join('');
 
 
 }
+
+function onSubmitForm() {
+   const elEmail = document.querySelector('input[name=email]');
+const elText = document.querySelector('input[name=text]');
+ const elContentMsg =document.getElementById("textarea");
+
+
+const email = elEmail.value;
+const text = elText.value;
+const content = elContentMsg.value; 
+
+return window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=neomibuz@gmail.com&su=${text}&body=${content}${email}&bcc=someone.else@example.com`)
+ 
+}
+
+
+
+//  `<div class="portfolio-modal modal fade" id="portfolioModal${index+1}" tabindex="-1" role="dialog" aria-hidden="true">
+// <div class="modal-dialog">
+//   <div class="modal-content">
+//     <div class="close-modal" data-dismiss="modal">
+//       <div class="lr">
+//         <div class="rl"></div>
+//       </div>
+//     </div>
+//     <div class="container">
+//       <div class="row">
+//         <div class="col-lg-8 mx-auto">
+//           <div class="modal-body">
+            
+//             <h2>${project.name}</h2>
+//             <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+//             <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
+//             <p>
+//             </p>
+//             <ul class="list-inline">
+//               <li>Date: January 2017</li>
+//               <li>Client: Threads</li>
+//               <li>Category: Illustration</li>
+//             </ul>
+//             <button type="button" class="btn btn-success" ><a href="${project.url}" target="_blank">Check it Out</button>
+//             <button class="btn btn-primary" data-dismiss="modal" type="button">
+//                 <i class="fa fa-times"></i>
+//                 Close Project</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+// </div>  `
+
+
 
 
 
@@ -157,3 +209,66 @@ function renderModal() {
 
 
 // onclick=href="onOpenModal(${project.id})"
+
+
+
+
+// `
+//         <div class="container">
+//         <div
+//           class="portfolio-modal modal fade"
+//           id="portfolioModal${indx+1}"
+//           tabindex="-1"
+//           role="dialog"
+//           aria-hidden="true"
+//         >
+//           <div class="modal-dialog">
+//             <div class="modal-content">
+//               <div class="close-modal" data-dismiss="modal">
+//                 <div class="lr">
+//                   <div class="rl"></div>
+//                 </div>
+//               </div>
+//               <div class="container">
+//                 <div class="row">
+//                   <div class="col-lg-8 mx-auto">
+//                     <div class="modal-body">
+                     
+//                       <h2>${project.name}</h2>
+//                       <p class="item-intro text-muted">
+//                       ${project.desc}
+//                       </p>
+                     
+//                       <img
+//                         class="img-fluid d-block mx-auto"
+//                         src="${project.imgSrc}"
+//                         alt=""
+//                       />
+//                       <p>
+//                       ${project.desc}
+                       
+//                       </p>
+//                       <ul class="list-inline">
+//                         <li>Date: January 2017</li>
+//                         <li>Date: January 2017</li>
+//                         </ul>
+//        <button type="button" class="btn btn-success" ><a href="${project.url}" target="_blank">Check it Out</button>
+                     
+                      
+//                       <button
+//                         class="btn btn-primary"
+//                         data-dismiss="modal"
+//                         type="button"
+//                       >
+//                         <i class="fa fa-times"></i>
+//                         Close Project
+//                       </button>
+                  
+//                     </div>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>`
--- js/gallery-service.js
+++ js/gallery-service.js
@@ -11,13 +11,13 @@ function creatProjects() {
 
             creatProject('inPictur', 'inPictur', 'Pic Game', 'how in the picture?', 'proj/inPicture', 'proj/inPicture/picture/1.jpg'),
 
-            creatProject('touchnums', 'Touch-Nums', 'Touch-Nums', 'touch fast', 'proj/touch-nums', 'proj/touch-nums/img/toch nums.png'),
+            creatProject('touchnums', 'Touch-Nums', 'Touch-Nums', 'touch fast', 'proj/touch-nums/', 'proj/touch-nums/img/toch nums.png'),
 
-            creatProject('book-shop', 'book-shop', 'book-shop', 'a book-shop proj', 'proj/book-shop', 'proj/book-shop/img/peter pan.jpg'),
+            creatProject('book-shop', 'book-shop', 'book-shop', 'a book-shop proj', 'proj/book-shop/', 'proj/book-shop/img/peter pan.jpg'),
 
-            creatProject('pacman', 'Pacman', 'Pacman', 'play the pacman game', 'proj/pacman-inClass', 'proj/pacman-inClass/img/pacman.png'),
+            creatProject('pacman', 'Pacman', 'Pacman', 'play the pacman game', 'proj/pacman-inClass/', 'proj/pacman-inClass/img/pacman.png'),
 
-            creatProject('minesweeper', 'minesweeper', 'minesweeper', 'Mine Sweeper Game', 'proj/minesweeper-pro1', 'proj/minesweeper-pro1/img/minesweeper.png')
+            creatProject('minesweeper', 'minesweeper', 'minesweeper', 'Mine Sweeper Game', 'proj/minesweeper-pro1/', 'proj/minesweeper-pro1/img/minesweeper.png')
         ]
     }
 }
