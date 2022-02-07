0942bdb83dd03999e693fff9045b61a9a39b15b2
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-02-07 14:16:17 +0200

    abaut me

---
+++ js/proj-service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+
+function creatPortfolio() {
+    return [
+        {
+            id: "sokoban",
+            name: "Sokoban",
+            title: "Better push those boxes",
+            desc: "lorem ipsum lorem ipsum lorem ipsum",
+            url: "projs/sokoban",
+            publishedAt: 1448693940000,
+            labels: ["Matrixes", "keyboard events"],
+        },
+    ]
+}
