0622a5c0149a07d78f18b9c7ef46e1b205cc6e67
neomi buzaglo yoresh <neomibuz@gmail.com>
2022-02-08 00:11:36 +0200

    last up

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -44,66 +44,58 @@ function renderModal() {
     var strHTMLs = gProjs.map((project,indx) => {
         return `
         <div class="container">
-        <div
-          class="portfolio-modal modal fade"
-          id="portfolioModal${indx+1}"
-          tabindex="-1"
-          role="dialog"
-          aria-hidden="true"
-        >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="close-modal" data-dismiss="modal">
+            <div
+                class="portfolio-modal modal fade"
+                id="portfolioModal${indx+1}"
+                tabindex="-1"
+                role="dialog"
+                aria-hidden="true"
+            >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal">
                 <div class="lr">
-                  <div class="rl"></div>
+                <div class="rl"></div>
                 </div>
-              </div>
-              <div class="container">
-                <div class="row">
-                  <div class="col-lg-8 mx-auto">
-                    <div class="modal-body">
-                     
-                      <h2>${project.name}</h2>
-                      <p class="item-intro text-muted">
-                      ${project.desc}
-                      </p>
-                     
-                      <img
-                        class="img-fluid d-block mx-auto"
-                        src="${project.imgSrc}"
-                        alt=""
-                      />
-                      <p>
-                      ${project.desc}
-                       
-                      </p>
-                      <ul class="list-inline">
-                        <li>Date: January 2017</li>
-                        <li>Date: January 2017</li>
-                        </ul>
-      
-       
-                        <a href="${project.url}" target="_blank">
-                         <button type="button" class="btn btn-success" >Check it Out</button>
-                         </a>
-                     
-                      
-                      <button
-                        class="btn btn-primary"
-                        data-dismiss="modal"
-                        type="button"
-                      >
-                        <i class="fa fa-times"></i>
-                        Close Project
-                      </button>
-                  
+                </div>
+                 <div class="container">
+                 <div class="row">
+                 <div class="col-lg-8 mx-auto">
+                 <div class="modal-body">
+                        
+                 <h2>${project.name}</h2>
+                 <p class="item-intro text-muted">
+                 ${project.desc}
+                 </p>
+                        
+                 <img
+                 class="img-fluid d-block mx-auto" src="${project.imgSrc}"/>
+                 <p>
+                 ${project.desc}
+                        
+                 </p>
+                 <ul class="list-inline">
+                    <li>Date: January 2017</li>
+                    <li>Date: January 2017</li>
+                 </ul>
+        
+        
+                <a href="${project.url}" target="_blank">
+                <button type="button" class="btn btn-success" >Check it Out</button>
+                </a>
+                        
+                        
+                <button class="btn btn-primary" data-dismiss="modal" type="button" >
+                            <i class="fa fa-times"></i>
+                            Close Project
+                        </button>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
       </div>`});
 
     document.querySelector('.modal-container').innerHTML = strHTMLs.join('');
